c80a21fd0363d21d8acfc2c08439d24ec86925a7
RedPenguin101 <allaboutthatmace1789@gmail.com>
2020-08-07 16:10:56 +0100

    refactor

--- src/core.clj
+++ src/core.clj
@@ -1,38 +1,37 @@
 (ns modeldsl.core)
 
-(defn profile-period-lookup [profile key period]
+(def sum +)
+(def product *)
+(def increment inc)
+
+(defn previous [{previous-periods :previous-periods} key]
+  (when (not-empty previous-periods)
+    (key (last previous-periods))))
+
+(defn this [{new-period :new-period} key]
+  (key new-period))
+
+(defn from-profile [{profile :profile} key]
+  (key profile))
+
+(defn profile-period-lookup [{profile :profile} key period]
   (or (first (keep (fn [[start end value]]
                      (when (some #{period} (range start (inc end)))
                        value))
                    (key profile))) 0))
 
-(defn previous [previous-periods key]
-  (when (not-empty previous-periods)
-    (key (last previous-periods))))
+(def put-ins #{'previous 'this 'from-profile 'profile-period-lookup})
 
-(defn this [new-period key]
-  (key new-period))
-
-(defn from-profile [profile key]
-  (key profile))
-
-(defn interpret [function options]
+(defn- interpret [function options]
   (if (coll? function)
     (let [[operator & operands] function]
-      (println operator)
       (cond
-        (= (count function) 1)              (interpret operator options)
-        (= operator 'previous)              (previous (:previous-periods options) (interpret operands options))
-        (= operator 'this)                  (this (:new-period options) (interpret operands options))
-        (= operator 'from-profile)          (from-profile (:profile options) (interpret operands options))
-        (= operator 'profile-period-lookup) (apply profile-period-lookup (:profile options)
-                                                   (map #(interpret % options) operands))
-        :else                               (apply operator (map #(interpret % options) operands))))
+        (= (count function) 1) (interpret operator options)
+        (put-ins operator)     (apply (eval operator) options (map #(interpret % options) operands))
+        :else                  (apply (eval operator) (map #(interpret % options) operands))))
     function))
 
-(= (quote previous) 'previous)
-
-(defn do-row [[key function default] profile previous-periods new-period]
+(defn- do-row [[key function default] profile previous-periods new-period]
   {key (if (and default (empty? previous-periods))
          default
          (interpret function {:profile          profile
@@ -45,49 +44,24 @@
                   (merge new-period (do-row model-row profile previous-periods new-period)))
                 {}
                 model)))
-
-(def model
-  [[:period-number
-    [inc ['previous :period-number]] 0]
-   [:starting-aum
-    ['previous :ending-aum] 0]
-   [:drawdown
-    [* ['from-profile :commitments]
-     [nth ['from-profile :contributions] ['this :period-number] 0]]]
-   [:pnl
-    [* ['this :starting-aum] ['from-profile :return]]]
-   [:distribution
-    [* -1 ['this :starting-aum] ['profile-period-lookup :distributions ['this :period-number]]]]
-   [:ending-aum
-    [+ ['this :drawdown] ['this :starting-aum] ['this :pnl] ['this :distribution]]]])
-
-(def model2
-  [[:period-number
-    [inc ['previous :period-number]] 0]])
-
-(def roll (partial next-period model {:commitments   100
-                                      :contributions [0.25 0.25 0.25 0.25]
-                                      :return        0.2
-                                      :distributions [[0 3 0]
-                                                      [4 10 1/10]]                                      }))
-
-(last (take 5 (iterate roll [])))
-
-(name 'previous)
-
 (comment
-  (def fund-profile {:commitments   100000
-                     :contributions [1/4 1/4 1/4 1/4]
-                     :return        0.2
-                     :distributions [[0 3 0]
-                                     [4 10 1/10]]})
+  (def model
+    '[[:period-number (increment (previous :period-number)) 0]
+      [:starting-aum  (previous :ending-aum) 0]
+      [:drawdown      (product (from-profile :commitments) (nth (from-profile :contributions) (this :period-number) 0))]
+      [:pnl           (product (this :starting-aum) (from-profile :return))]
+      [:distribution  (product -1 (this :starting-aum) (profile-period-lookup :distributions (this :period-number)))]
+      [:ending-aum    (sum (this :drawdown) (this :starting-aum) (this :pnl) (this :distribution))]])
 
   (def model
-    [[:period-number (inc (previous :period-number)) 0]
-     [:starting-aum  (previous :ending-aum) 0]
-     [:drawdown      (* (:commitments profile)
-                        ((this :period-number) (:contributions profile)))]
-     [:pnl           (* (:return profile) (this :starting-aum))]
-     [:distribution  (* (+ (this :starting-aum) (this :pnl) (this :drawdown))
-                        (profile-lookup :distributions (this :period-number)))]
-     [:ending-aum    (+ (this :starting-aum) (this :drawdown) (this :pnl) (this :distribution))]]))
+    '[(:period-number (inc (previous :period-number)) 0)])
+
+  (def profile {:commitments   100
+                :contributions [0.25 0.25 0.25 0.25]
+                :return        0.2
+                :distributions [[0 3 0]
+                                [4 10 1/10]]})
+
+  (last (take 15 (iterate (partial next-period model profile)
+                          []))))
+
