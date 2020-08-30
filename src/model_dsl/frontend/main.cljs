e0ffa5d544a81e9416390b09013939f5a058ce55
RedPenguin101 <allaboutthatmace1789@gmail.com>
2020-08-30 13:08:14 +0100

    refactor: rename model-row to measure throughout

--- src/model_dsl/frontend/main.cljs
+++ src/model_dsl/frontend/main.cljs
@@ -23,13 +23,13 @@
 (defn- keywordize [form]
   (postwalk #(if (symbol? %) (keyword %) %) form))
 
-(defn- extract-code [model-rows]
+(defn- extract-code [model]
   (reduce (fn [A [name value]]
             (if (map? value)
               (assoc A name (:code value))
               (assoc A name value)))
           {}
-          model-rows))
+          model))
 
 (defn- decimal-format [num]
   (let [rounded (Math/round num)]
@@ -55,35 +55,35 @@
 ;; EVENTS
 
 (rf/reg-event-db
-  :update-current-model-row
+  :update-selected-measure
   (fn [db [_ {:keys [name code name-in-model]}]]
     (let [code (or code (get-in db [:model-rows name :string-rep]))]
-      (println "update current fired with" name code)
+      (println "update-selected-measure fired with" name code)
       (assoc db :current-model-row
              {:name name :code code :name-in-model name-in-model}))))
 
 (rf/reg-event-db
- :create-new-model-row
+ :create-new-measure
  (fn [db [_ name]]
    (-> db
        (assoc-in [:model-rows name] {:code nil :string-rep ""})
        (update :row-order conj name))))
 
 (rf/reg-event-db
-  :update-model-row
+  :update-measure
   (fn [db [_ {:keys [name code string-rep]}]]
     (assoc-in db [:model-rows name] {:code       code
                                      :string-rep string-rep})))
 
 (rf/reg-event-db
- :remove-model-row
+ :remove-measure
  (fn [db [_ name]]
-   (update db :row-order (fn [model-rows] (into [] (remove #(= % name) model-rows))))))
+   (update db :row-order (fn [measures] (into [] (remove #(= % name) measures))))))
 
 (rf/reg-event-db
- :change-model-row-order
+ :change-measure-order
  (fn [db [_ new-order]]
-   (println "change-model-row-order fired with" new-order)
+   (println "change-measure-order fired with" new-order)
    (assoc db :row-order (vec new-order))))
 
 (rf/reg-event-db
@@ -96,17 +96,17 @@
 (rf/reg-sub :all (fn [db _] db))
 
 (rf/reg-sub
-  :model-rows
+  :measures
   (fn [db _]
     (:model-rows db)))
 
 (rf/reg-sub
-  :current-model-row
+  :selected-measure
   (fn [db _]
     (:current-model-row db)))
 
 (rf/reg-sub
-  :model-row-order
+  :measure-order
   (fn [db _]
     (:row-order db)))
 
@@ -126,8 +126,8 @@
         [:div.box
          [:h3.title.is_h3 "Enter new model row name"]
          [:form {:on-submit #(do (.preventDefault %)
-                                 (rf/dispatch [:create-new-model-row (keywordify-measure-name @new-row-name)])
-                                 (rf/dispatch [:update-current-model-row {:name (keywordify-measure-name @new-row-name)}])
+                                 (rf/dispatch [:create-new-measure (keywordify-measure-name @new-row-name)])
+                                 (rf/dispatch [:update-selected-measure {:name (keywordify-measure-name @new-row-name)}])
                                  (reset! modal-active? false)
                                  (reset! new-row-name nil))}
           [:div.field
@@ -144,8 +144,8 @@
   (let [s (r/atom {})
         modal-active? (r/atom false)]
     (fn []
-      (let [measures         @(rf/subscribe [:model-row-order])
-            selected-measure @(rf/subscribe [:current-model-row])]
+      (let [measures         @(rf/subscribe [:measure-order])
+            selected-measure @(rf/subscribe [:selected-measure])]
         [:div
          #_[:div.dev {:style {:border "1px solid red" :text "0.8em"}} @s]
          [:div.dropdown {:class (when (:dropdown-active @s) :is-active)}
@@ -160,7 +160,7 @@
              (for [measure measures]
                [:a.dropdown-item
                 {:class (when (= measure (:name selected-measure)) :is-active)
-                 :on-click #(rf/dispatch [:update-current-model-row {:name measure}])
+                 :on-click #(rf/dispatch [:update-selected-measure {:name measure}])
                  :style {:border-top (when (= measure (:drag-over @s)) "1px solid blue")
                          :width 300
                          :display :flex
@@ -169,7 +169,7 @@
                  :draggable true
                  :on-drag-start #(swap! s assoc :drag-item measure)
                  :on-drag-end (fn [_]
-                                (rf/dispatch [:change-model-row-order
+                                (rf/dispatch [:change-measure-order
                                               (vec-reorder measures (:drag-over @s) (:drag-item @s))])
                                 (swap! s dissoc :drag-item :drag-over))
                  :on-drag-over (fn [e]
@@ -178,8 +178,8 @@
                  :on-drag-leave #(swap! s assoc :drag-over :nothing)}
                 [:span (name measure)]
                 [:span.icon.is-small
-                 {:on-click #(do (rf/dispatch [:remove-model-row measure])
-                                 (rf/dispatch [:update-current-model-row {:name (first measures)}])
+                 {:on-click #(do (rf/dispatch [:remove-measure measure])
+                                 (rf/dispatch [:update-selected-measure {:name (first measures)}])
                                  (swap! s update :dropdown-active not)
                                  (.stopPropagation %))}
                  [:i.fas.fa-backspace]]])
@@ -193,7 +193,7 @@
 
 (defn codemirror-model []
   (fn []
-    (let [{:keys [name code]} @(rf/subscribe [:current-model-row])]
+    (let [{:keys [name code]} @(rf/subscribe [:selected-measure])]
       [:div {:style {:border        (if (valid-edn? code)
                                       "1px solid #00d1b2"
                                       "1px solid red")
@@ -206,13 +206,13 @@
        [:> UnControlled
         {:value     code
          :options   {:mode "clojure"}
-         :on-change (fn [_ _ v] (rf/dispatch [:update-current-model-row
+         :on-change (fn [_ _ v] (rf/dispatch [:update-selected-measure
                                               {:name name
                                                :code v}]))}]])))
 
 (defn model-window []
-  (let [row-order         @(rf/subscribe [:model-row-order])
-        current-selection @(rf/subscribe [:current-model-row])]
+  (let [row-order         @(rf/subscribe [:measure-order])
+        current-selection @(rf/subscribe [:selected-measure])]
     [:div
      [codemirror-model]
      [:div.container {:style {:margin-top 10}}
@@ -222,7 +222,7 @@
                     (.preventDefault e)
                     (when (valid-edn? (:code current-selection))
                       (rf/dispatch
-                       [:update-model-row
+                       [:update-measure
                         {:name       (:name current-selection)
                          :code       (keywordize
                                       (edn/read-string (:code current-selection)))
@@ -259,8 +259,8 @@
 
 (defn output-window []
   (let [profile    (edn/read-string @(rf/subscribe [:profile]))
-        row-order @(rf/subscribe [:model-row-order])
-        code (extract-code @(rf/subscribe [:model-rows]))]
+        row-order @(rf/subscribe [:measure-order])
+        code (extract-code @(rf/subscribe [:measures]))]
     (if-let [scenario (try-model (for [measure row-order]
                                    [measure (measure code)])
                               profile
@@ -277,7 +277,7 @@
               (let [measure-name (first row)]
                 [:td {:style {:white-space :nowrap}
                       :on-click 
-                      #(rf/dispatch [:update-current-model-row
+                      #(rf/dispatch [:update-selected-measure
                                      {:name (keyword measure-name)}])}
                  (stringify-measure-name measure-name)])
               (for [v (rest row)]
